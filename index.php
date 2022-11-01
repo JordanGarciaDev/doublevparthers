dc80fe3a567fedb8a0373ed5cc57466ba53204e3
Jordan Garcia <52794915+JordanGarciaDev@users.noreply.github.com>
2022-10-31 22:09:35 -0500

    index

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+$titulo = "JordanGarciaDev | GitHub buscar usuario de Github";
+$mode_text = "DARK";
+$no_results = "No existe";
+$no_cumple = "No cumple criterio";
+$text_search = "Buscar";
+$text_repo = "Repositorios";
+$text_Seguidores = "Seguidores";
+$text_Siguiendo = "Siguiendo";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,16 +19,16 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet">
-  <title>JordanGarciaDev | GitHub buscar usuario de Github</title>
+  <title><?php echo $titulo;?></title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
   <div class="container">
     <header class="header">
-      <img src="https://www.doublevpartners.com/assets/images/shared/dvp-logo.svg" alt="">
+      <img src="https://www.doublevpartners.com/assets/images/shared/dvp-logo.svg" alt="logo">
       <div id="btn-modo">
-        <p id="mode-text">DARK</p>
+        <p id="mode-text"><?php echo $mode_text;?></p>
         <div class="icon-container"><img id="mode-icon" src="./assets/icon-moon.svg" alt=""></div>
       </div>
     </header>
@@ -27,17 +38,16 @@
         <div class="buscar-container active">
           <input type="search" name="user-input" id="usuario" minlength="4" placeholder="Buscar usuario de Github...">
           <div class="error">
-            <p id="no-results">No existe</p>
-            <p id="no-cumple">No cumple criterio</p>
-
+            <p id="no-results"><?php echo $no_results;?></p>
+            <p id="no-cumple"><?php echo $no_cumple;?></p>
           </div>
-          <button class="btn-buscar" id="submit">Buscar</button>
+          <button class="btn-buscar" id="submit"><?php echo $text_search;?></button>
         </div>
         <div class="perfil-container">
           <div class="perfil-content">
             <div class="perfil-header">
               <a href="#" id="linkPerfil">
-                <img id="avatar" src="./assets/favicon-32x32.png" alt="">
+                <img id="avatar" src="./assets/favicon-32x32.png" alt="icono default">
               </a>
               <div class="perfil-info-wrapper">
                 <div class="perfil-nombre">
@@ -52,15 +62,15 @@
             <p id="bio"></p>
             <div class="perfil-stats-wrapper">
               <div class="perfil-stat">
-                <p class="titulo-stat">Repositorios</p>
+                <p class="titulo-stat"><?php echo $text_repo;?></p>
                 <p id="repos"class="valor-stat"></p>
               </div>
               <div class="perfil-stat">
-                <p class="titulo-stat">Seguidores</p>
+                <p class="titulo-stat"><?php echo $text_Seguidores;?></p>
                 <p id="followers" class="valor-stat"></p>
               </div>
               <div class="perfil-stat">
-                <p class="titulo-stat">Siguiendo</p>
+                <p class="titulo-stat"><?php echo $text_Siguiendo;?></p>
                 <p id="following" class="valor-stat"></p>
               </div>
             </div>
