8e96c9ee759dfb966c7d19b0450e5b99f286baac
Jordan Garcia <52794915+JordanGarciaDev@users.noreply.github.com>
2022-11-14 14:44:37 -0500

    base de datos

--- APIS/tickets.sql
+++ APIS/tickets.sql
@@ -1,40 +1,40 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 10.4.11-MariaDB : Database - doublevparthers
+*********************************************************************
+*/
 
+/*!40101 SET NAMES utf8 */;
 
-CREATE TABLE `items` (
-  `id` int(11) NOT NULL,
-  `usuario` varchar(256) NOT NULL,
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`doublevparthers` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `doublevparthers`;
+
+/*Table structure for table `tickets` */
+
+DROP TABLE IF EXISTS `tickets`;
+
+CREATE TABLE `tickets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(100) NOT NULL,
   `descripcion` text NOT NULL,
-  `estatus` int(255) NOT NULL,
-  `category_id` int(11) NOT NULL,
+  `estatus` varchar(1) NOT NULL DEFAULT '1',
   `fecha_creacion` datetime NOT NULL,
-  `fecha_actualizacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `fecha_actualizacion` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
+/*Data for the table `tickets` */
 
-INSERT INTO `items` (`id`, `usuario`, `descripcion`, `estatus`, `category_id`, `fecha_creacion`, `fecha_actualizacion`) VALUES
-(1, 'LG P880 4X HD', 'My first awesome phone!', 336, 3, '2014-06-01 01:12:26', '2014-05-31 17:42:26'),
-(2, 'Google Nexus 4', 'The most awesome phone of 2013!', 299, 2, '2014-06-01 01:12:26', '2014-05-31 17:42:26'),
-(3, 'Samsung Galaxy S4', 'How about no?', 600, 3, '2014-06-01 01:12:26', '2014-05-31 17:42:26'),
-(6, 'Bench Shirt', 'The best shirt!', 29, 1, '2014-06-01 01:12:26', '2014-05-31 02:42:21'),
-(7, 'Lenovo Laptop', 'My business partner.', 399, 2, '2014-06-01 01:13:45', '2014-05-31 02:43:39'),
-(8, 'Samsung Galaxy Tab 10.1', 'Good tablet.', 259, 2, '2014-06-01 01:14:13', '2014-05-31 02:44:08'),
-(9, 'Spalding Watch', 'My sports watch.', 199, 1, '2014-06-01 01:18:36', '2014-05-31 02:48:31'),
-(10, 'Sony Smart Watch', 'The coolest smart watch!', 300, 2, '2014-06-06 17:10:01', '2014-06-05 18:39:51'),
-(11, 'Huawei Y300', 'For testing purposes.', 100, 2, '2014-06-06 17:11:04', '2014-06-05 18:40:54'),
-(12, 'Abercrombie Lake Arnold Shirt', 'Perfect as gift!', 60, 1, '2014-06-06 17:12:21', '2014-06-05 18:42:11'),
-(13, 'Abercrombie Allen Brook Shirt', 'Cool red shirt!', 70, 1, '2014-06-06 17:12:59', '2014-06-05 18:42:49'),
-(26, 'Another product', 'Awesome product!', 555, 2, '2014-11-22 19:07:34', '2014-11-21 21:37:34'),
-(28, 'Wallet', 'You can absolutely use this one!', 799, 6, '2014-12-04 21:12:03', '2014-12-03 23:42:03'),
-(31, 'Amanda Waller Shirt', 'New awesome shirt!', 333, 1, '2014-12-13 00:52:54', '2014-12-12 03:22:54'),
-(42, 'Nike Shoes for Men', 'Nike Shoes', 12999, 3, '2015-12-12 06:47:08', '2015-12-12 07:17:08'),
-(48, 'Bristol Shoes', 'Awesome shoes.', 999, 5, '2016-01-08 06:36:37', '2016-01-08 07:06:37'),
-(60, 'Rolex Watch', 'Luxury watch.', 25000, 1, '2016-01-11 15:46:02', '2016-01-11 16:16:02');
+insert  into `tickets`(`id`,`usuario`,`descripcion`,`estatus`,`fecha_creacion`,`fecha_actualizacion`) values (1,'jordan','Ticket Test #1','1','2022-11-01 01:12:26','2022-11-13 13:10:24'),(2,'angie','Ticket Test #2','1','2022-11-01 01:12:26','2022-11-14 13:11:20'),(3,'jaider','Ticket Test #3','1','2022-11-01 01:12:26','2022-11-14 13:11:52'),(4,'masha','Este es un ticket en estado abierto','1','2022-11-14 19:50:04','2022-11-14 13:57:46'),(8,'gorila','Este es un ticket en estado cerrado','1','2022-11-14 19:59:48','2022-11-14 14:19:59'),(9,'doublevparthers','Este es un ticket de prueba actualizado, somos doublevparthers','0','2022-11-14 20:30:22','2022-11-14 14:30:22');
 
-
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
